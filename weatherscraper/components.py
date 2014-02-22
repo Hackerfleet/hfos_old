9111555f08267f8a14eed41451a8790b62761e26
riot <riot@hackerfleet.org>
2014-02-22 03:19:19 +0100

    Nice filters added from ijon's code. Wrapped up in components and strapped together, still in components.py Will build page assembler and rearrange daemon tomorrow.

--- weatherscraper/components.py
+++ weatherscraper/components.py
@@ -7,12 +7,14 @@ import urllib.request
 import urllib.error
 import socket
 
+
 from Axon.Ipc import producerFinished, shutdownMicroprocess
 from Axon.ThreadedComponent import threadedcomponent
-from Axon.Component import component
 from Kamaelia.Chassis.Graphline import Graphline
+from Kamaelia.Chassis.Pipeline import Pipeline
 from Kamaelia.Util.Clock import CheapAndCheerfulClock as SimpleClock
 from Kamaelia.Util.Console import ConsoleEchoer
+from Kamaelia.Util.Filter import Filter
 
 
 class HTTPGetter(threadedcomponent):
@@ -129,82 +131,86 @@ class HTTPGetter(threadedcomponent):
             time.sleep(0.1)
 
 
-class WeatherFilter(component):
-    """
-    
-    """
+class Wetter24Filter(object):
+    """Filters out interesting info from wetter24.de"""
 
-    Inboxes = {
-        "inbox": "Messages to be passed through",
-        "control": "Shutdown signalling",
+    # TODO: Make sure, unicode is handled correctly
+    # TODO: Strip of unnecessary data
+
+    def filter(self, newtext):
+        local_weather = newtext[newtext.find('head_ww'):newtext.find('hPa<') + 4]
+
+        condition_zahl = local_weather.find('head_ww')
+        condition_begin = local_weather.find('alt="', condition_zahl) + 5
+        condition_end = local_weather.find('"', condition_begin)
+        condition = local_weather[condition_begin:condition_end]
+
+        temp_zahl = local_weather.find('head_tt2')
+        temp_begin = local_weather.find('>', temp_zahl) + 1
+        temp_end = local_weather.find('</p>', temp_zahl)
+        temp = local_weather[temp_begin:temp_end]
+
+        wind_zahl = local_weather.find('wind32px')
+        wind_begin = local_weather.find('alt="', wind_zahl) + 5
+        wind_end = local_weather.find('" title', wind_zahl)
+        wind = local_weather[wind_begin:wind_end]
+
+        w_speed_zahl = local_weather.find('head_ff2')
+        w_speed_begin = local_weather.find('>', w_speed_zahl) + 1
+        w_speed_end = local_weather.find('</p>', w_speed_zahl)
+        w_speed = local_weather[w_speed_begin:w_speed_end]
+
+        press_zahl = local_weather.find('head_ppp2')
+        press_begin = local_weather.find('>', press_zahl) + 1
+        press_end = local_weather.find('</p>', press_begin)
+        press = local_weather[press_begin:press_end]
+
+        results = {'condition': condition,
+                   'temperature': temp,
+                   'wind_dir': wind,
+                   'wind_speed': w_speed,
+                   'pressure': press
+        }
+        return results
+
+
+class NasaSDOFilter(object):
+    """Filters out interesting info from wetter24.de"""
+
+    def filter(self, newtext):
+        snippet_solar_begin = newtext.find('<h1>AIA 094, 335, 193</h1>')
+        snippet_solar = newtext[snippet_solar_begin:newtext.find('latest_image', snippet_solar_begin) + 9]
+        solar_img_begin = snippet_solar.find('href="') + 6
+        solar_img_end = snippet_solar.find('">', solar_img_begin)
+        solar_img = 'http://sdo.gsfc.nasa.gov' + snippet_solar[solar_img_begin:solar_img_end]
+
+        return {'url_image_nasa_sdo': solar_img}
+
+
+def siteScraper(url, interval, filter):
+    """Constructs a sitescraper containing a SimpleClock, HTTPGetter and Filter"""
+
+    return Pipeline(SimpleClock(interval),
+                    HTTPGetter(url),
+                    Filter(filter)
+    )
+
+
+weatherScraper = Graphline(
+    W24=siteScraper("http://www.wetter24.de/wetter/berlin-alexanderplatz/10389.html",
+                    interval=15,
+                    filter=Wetter24Filter()
+    ),
+    NSDOIMG=siteScraper("http://sdo.gsfc.nasa.gov/data/",
+                        interval=23,
+                        filter=NasaSDOFilter()
+    ),
+
+    CE=ConsoleEchoer(),
+    linkages={
+        ("W24", "outbox"): ("CE", "inbox"),
+        ("NSDOIMG", "outbox"): ("CE", "inbox")
     }
-    Outboxes = {
-        "outbox": "Passed through messages",
-        "signal": "Shutdown signalling",
-    }
-
-    def __init__(self):
-        """\
-        """
-        super(WeatherFilter, self).__init__()
-
-
-    def main(self):
-        """\
-        """
-        shutdown = False
-
-        while not shutdown:
-            print("Cycling")
-            while not self.anyReady():
-                yield 1
-
-            if self.dataReady("inbox"):
-                print("Incoming")
-
-                data = self.recv("inbox")
-
-                print("Filtering")
-
-                local_weather = data[data.find('head_ww'):data.find('hPa<') + 4]
-
-                condition_zahl = local_weather.find('head_ww')
-                condition_begin = local_weather.find('alt="', condition_zahl) + 5
-                condition_end = local_weather.find('"', condition_begin)
-                condition = local_weather[condition_begin:condition_end]
-
-                #print(condition)
-
-                snippet_solar_begin = data.find('<h1>AIA 094, 335, 193</h1>')
-                snippet_solar = data[snippet_solar_begin:data.find('latest_image', snippet_solar_begin) + 9]
-
-                #print(snippet_solar)
-
-                self.send(condition + snippet_solar)
-                yield 1
-
-            time.sleep(0.1)
-
-            if self.dataReady("control"):
-                shutdown = True
-
-        for msg in self.Inbox("control"):
-            self.send(msg, "signal")
-
-
-weatherScraper = Graphline(W24=HTTPGetter(url="http://www.wetter24.de/wetter/berlin-alexanderplatz/10389.html"),
-                           N=HTTPGetter(url="http://sdo.gsfc.nasa.gov/data/"),
-                           W24C=SimpleClock(5),
-                           NC=SimpleClock(10),
-                           FILTER=WeatherFilter(),
-                           CE=ConsoleEchoer(),
-                           linkages={
-                               ("W24C", "outbox"): ("W24", "inbox"),
-                               ("NC", "outbox"): ("N", "inbox"),
-                               ("W24", "outbox"): ("FILTER", "inbox"),
-                               ("N", "outbox"): ("FILTER", "inbox"),
-                               ("FILTER", "outbox") : ("CE", "inbox")
-                           }
 )
 
 weatherScraper.run()
